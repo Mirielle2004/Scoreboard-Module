16e907a1d21e103e57542d3b4c11de7003962140
Bar Galili <bureyburey@gmail.com>
2017-01-24 19:34:54 +0200

    Update
    added loader animation
    width/height customization of both dialogs

--- scoreboard.js
+++ scoreboard.js
@@ -12,84 +12,113 @@ see https://code.sololearn.com/Wb3UALD0atIG/ for tutorial
 if changing options, DO NOT REMOVE EXISTING options, just leave the {} brackets empty
 default options:
 var options={
+    // customize the following:
+    // syntax is like css, but make sure:
+    // key and value are both strings!
+    "scoreboardWidth":"auto", //recommended
+    "scoreboardHeight":"400", // recommended
+    "submitDialogWidth":"auto", //recommended
+    "submitDialogHeight":"auto", // recommended
          burey:{
+    // Scoreboard Module By Burey text
          "font-family":"Times New Roman",
          "font-style":"italic",
          "text-shadow": "5px 2px 4px grey",
-         "color": "green",
+         "color": "#9910ff",
          "background-color":"white",
          "border-radius": "25px"
          },
-         dialogTitle:{// dialogs title bar
+         dialogTitle:{
+         // dialogs title bar
+         "font-family":"Times New Roman",
          "background":"black",
-         "color":"white"
+         "color":"#9910ff"
          },
-         scoreboardContainer:{// scoreboard container dialog
-         "background-color":"#97B5F5"
+         scoreboardContainer:{
+    // scoreboard container dialog
+         "background-color":"black"
          },
-         tableHeader:{// # Name Score Time
+         tableHeader:{
+    // # Name Score Time
          "font-family":"Times New Roman",
-         "color": "black"
+         "color": "#9910ff"
          },
-         scoreRow:{//deprecated, customize each column instead
+         scorePosition:{
+    // #
          "font-family":"Times New Roman",
-         "color": "blue"
+         "color": "#9910ff"
          },
-         scorePosition:{// #
+         scoreName:{
+    // name of user
          "font-family":"Times New Roman",
-         "color": "blue"
+         "color": "#9910ff",
+         "word-wrap": "break-word",
+         "max-width":"110px"
          },
-         scoreName:{// name of user
-         "font-family":"'Press Start 2P', Times New Roman",
-         "font-size":"10px",
-         "color": "red"
-         },
-         scoreValue:{// score value
+         scoreValue:{
+    // score value
          "font-family":"Times New Roman",
          "font-size": "15px",
-         "color": "green"
+         "color": "#9910ff"
          },
-         scoreTime:{// time + date
+         scoreTime:{
+    // time + date
          "font-family":"Times New Roman",
          "font-size":"12px",
-         "color": "yellow"
+         "color": "#9910ff"
          },
-         newScoreContainer:{// submit new score dialog
-         "background-color":"#97B5F5"
+         newScoreContainer:{
+    // submit new score dialog
+         "background-color":"black"
          },
-         scoreYourScoreLabel:{// 'Your Score:' text in new score dialog
+         scoreYourScoreLabel:{
+    // 'Your Score:' text in new score dialog
          "font-family":"Times New Roman",
-         "color": "green"
+         "color": "#9910ff"
          },
-         scoreValueLabel:{// score value in new score dialog
+         scoreValueLabel:{
+    // score value in new score dialog
          "font-family":"Times New Roman",
-         "color": "blue",
+         "color": "#9910ff",
          "margin-right":"25px"
          },
-         scoreErrorLabel:{// error label in new score dialog
+         scoreErrorLabel:{
+    // error label in new score dialog
          "font-family":"Times New Roman",
          "color": "red"
          },
-         scoreboardButtons:{// scoreboard dialog buttons
+         scoreboardButtons:{
+    // scoreboard dialog buttons
          "font-family":"Times New Roman",
          "text-shadow": "5px 2px 4px grey",
-         "background":"red",
-         "color": "white",
-         "display":"none" // hide button
+         "background":"black",
+         "color": "#9910ff",
+         "display":"none" // remove button
          },
-         newScoreButtons:{// new score dialog buttons
+         newScoreButtons:{
+    // new score dialog buttons
         "font-family":"Times New Roman",
          "text-shadow": "5px 2px 4px grey",
          "background":"black",
-         "color": "white"
-         }}}
+         "color": "#9910ff"
+         },
+         loaderOptions:{
+     // scoreboard loading animation
+        "border-bottom": "16px solid #888",
+        "border-top": "16px solid #888",
+        "border-right": "16px solid #9910ff",
+        "border-left": "16px solid #9910ff",
+        "margin-left": "auto",
+        "margin-right": "auto"
+    }
+}
 usage example:
 
 var scoreboard = new Scoreboard(options);
 scoreboard.showScoreBoard(); // opens the scoreboard
 scoreboard.submitNewScoreDialog(scoreValue); // open score submit dialog (allow entry of name)
-
 */
+
 var markdown = {
     // taken from Michael Ermishin's Markdown module
     htmlEntitiesMap: {
@@ -107,7 +136,7 @@ var markdown = {
 };
 
 var optionsMap = {
-	// mapping of option name to actual element
+    // mapping of option name to actual element
     burey:"#by_burey",
     dialogTitle:".ui-dialog-titlebar",
     scoreboardContainer:"#container_scoreboard",
@@ -121,101 +150,138 @@ var optionsMap = {
     scoreValueLabel:"#lbl_best_score_submit",
     scoreErrorLabel:"#lbl_best_score_name_error",
     scoreboardButtons:".buttons_scoreboard",
-    newScoreButtons:".buttons_new_score"
+    newScoreButtons:".buttons_new_score",
+    loaderOptions:"#loader"
 };
 
 var defaultOptions = {
     // customize the following:
-    // syntax is css like, but make sure:
-    // key and value are both strings!
-         burey:{
-         "font-family":"Times New Roman",
-         "font-style":"italic",
-         "text-shadow": "5px 2px 4px grey",
-         "color": "green",
-         "background-color":"white",
-         "border-radius": "25px"
-         },
-         dialogTitle:{
+    // syntax is like css, but make sure:
+    // key and value are both strings inside each option!
+    "scoreboardWidth":"auto", //recommended
+    "scoreboardHeight":"400", // recommended
+    "submitDialogWidth":"auto", //recommended
+    "submitDialogHeight":"auto", // recommended
+    burey:{
+// Scoreboard Module By Burey text
+    "font-family":"Times New Roman",
+    "font-style":"italic",
+    "text-shadow": "5px 2px 4px grey",
+    "color": "green",
+    "background-color":"white",
+    "border-radius": "25px"
+    },
+    dialogTitle:{
     // dialogs title bar
-         "background":"black",
-         "color":"white"
-         },
-         scoreboardContainer:{
+    "background":"black",
+    "color":"white"
+    },
+    scoreboardContainer:{
     // scoreboard container dialog
-         "background-color":"#97B5F5"
-         },
-         tableHeader:{
+    "background-color":"#97B5F5"
+    },
+    tableHeader:{
     // # Name Score Time
-         "font-family":"Times New Roman",
-         "color": "black"
-         },
-         scoreRow:{
-    //deprecated, customize each column instead
-         "font-family":"Times New Roman",
-         "color": "blue"
-         },
-         scorePosition:{
+    "font-family":"Times New Roman",
+    "color": "black"
+    },
+    scorePosition:{
     // #
-         "font-family":"Times New Roman",
-         "color": "blue"
-         },
-         scoreName:{
+    "font-family":"Times New Roman",
+    "color": "blue"
+    },
+    scoreName:{
     // name of user
-         "font-family":"'Press Start 2P', Times New Roman",
-         "font-size":"10px",
-         "color": "red",
-         "word-wrap": "break-word",
-         "max-width":"110px"
-         },
-         scoreValue:{
+    "font-family":"'Press Start 2P', Times New Roman",
+    "font-size":"10px",
+    "color": "red",
+    "word-wrap": "break-word",
+    "max-width":"110px"
+    },
+    scoreValue:{
     // score value
-         "font-family":"Times New Roman",
-         "font-size": "15px",
-         "color": "green"
-         },
-         scoreTime:{
+    "font-family":"Times New Roman",
+    "font-size": "15px",
+    "color": "green"
+    },
+    scoreTime:{
     // time + date
-         "font-family":"Times New Roman",
-         "font-size":"12px",
-         "color": "yellow"
-         },
-         newScoreContainer:{
+    "font-family":"Times New Roman",
+    "font-size":"12px",
+    "color": "yellow"
+    },
+    newScoreContainer:{
     // submit new score dialog
-         "background-color":"#97B5F5"
-         },
-         scoreYourScoreLabel:{
+    "background-color":"#97B5F5"
+    },
+    scoreYourScoreLabel:{
     // 'Your Score:' text in new score dialog
-         "font-family":"Times New Roman",
-         "color": "green"
-         },
-         scoreValueLabel:{
+    "font-family":"Times New Roman",
+    "color": "green"
+    },
+    scoreValueLabel:{
     // score value in new score dialog
-         "font-family":"Times New Roman",
-         "color": "blue",
-         "margin-right":"25px"
-         },
-         scoreErrorLabel:{
+    "font-family":"Times New Roman",
+    "color": "blue",
+    "margin-right":"25px"
+    },
+    scoreErrorLabel:{
     // error label in new score dialog
-         "font-family":"Times New Roman",
-         "color": "red"
-         },
-         scoreboardButtons:{
+    "font-family":"Times New Roman",
+    "color": "red"
+    },
+    scoreboardButtons:{
     // scoreboard dialog buttons
-         "font-family":"Times New Roman",
-         "text-shadow": "5px 2px 4px grey",
-         "background":"red",
-         "color": "white",
-         "display":"none" // remove button
-         },
-         newScoreButtons:{
+    "font-family":"Times New Roman",
+    "text-shadow": "5px 2px 4px grey",
+    "background":"red",
+    "color": "white",
+    "display":"none" // remove button
+    },
+    newScoreButtons:{
     // new score dialog buttons
-        "font-family":"Times New Roman",
-         "text-shadow": "5px 2px 4px grey",
-         "background":"black",
-         "color": "white"
-         }
-      }
+    "font-family":"Times New Roman",
+    "text-shadow": "5px 2px 4px grey",
+    "background":"black",
+    "color": "white"
+    },
+    loaderOptions:{
+    // scoreboard loading animation
+        "border-bottom": "16px solid #888",
+        "border-top": "16px solid #888",
+        "border-right": "16px solid green",
+        "border-left": "16px solid green",
+        "margin-left": "auto",
+        "margin-right": "auto"
+    }
+}          
+
+function initializeLoader(){
+    // adds CSS to the scoreboard loading animation
+    var KeyFrame = {
+    // add spin keyframe rule to page
+     init: function(){
+         if(!KeyFrame.check){
+             //set the style and append to head
+             var css = $('<style>@keyframes spin{from {transform:rotate(0deg);}to {transform:rotate(360deg);}}</style>').appendTo('head'); //make sure you don't carriage return the css inline statement, or else it'll be error as ILLEGAL
+         //so u don't keep appending style to head
+         KeyFrame.check = true;
+        }
+     }
+    }
+    KeyFrame.init();
+    loaderOptions={
+        "border-bottom": "16px solid #888",
+        "border-top": "16px solid #888",
+        "border-right": "16px solid #66ccff",
+        "border-left": "16px solid #66ccff",
+        "border-radius": "70%",
+        "width": "50px",
+        "height": "50px",
+        "animation": "spin 1s linear infinite"
+    }
+    $('#loader').css(loaderOptions);
+}
       
       
 function Scoreboard(options = null, showErrors = false){
@@ -224,7 +290,10 @@ function Scoreboard(options = null, showErrors = false){
 // might need to add to a different div, depends on the code, it must be on topmost layer!
 
 // add container for the scoreboard
-  $("body").children().last().prepend("<div id='container_scoreboard' title='Scoreboard'><div id='by_burey'>Scoreboard Module By Burey</div><table id='scoreboard'><tr class='tbl_header'><th>#</th><th>Name</th><th>Score</th><th>Time</th></tr></table></div>");
+  $("body").children().last().prepend("<div id='container_scoreboard' title='Scoreboard'><div id='by_burey'>Scoreboard Module By Burey</div><table id='scoreboard'><tr class='tbl_header'><th>#</th><th>Name</th><th>Score</th><th>Time</th></tr></table><div id='loader'></div></div>");
+ try{
+ initializeLoader();
+}catch(err){}
 
 // add container div for the new score dialog
   $("body").children().last().prepend("<div id='container_submit_score_dialog' title='Submit Your Score!'><input placeholder='Name:' id='name_submit_new_score'><br /><label id='lbl_your_score'>Your Score: </label><label id=lbl_best_score_submit></label><br /><label id='lbl_best_score_name_error'></label></div>");
@@ -234,17 +303,16 @@ function Scoreboard(options = null, showErrors = false){
         options = defaultOptions;
     }
     var applyStyling = function(){
-    //change options your own customization
+    // apply CSS rules to elements
         for(var option in options){
-            //alert(option)
             try{
                 $(optionsMap[option]).css(options[option]);
              }catch(err){
-		     if(showErrors)
-		     	alert(err);
-	     }
+                 if(showErrors) alert(err);
+            }
         }
     }
+    
     // force websockets to prevent XMLHttpRequest console log
     firebase.database.INTERNAL.forceWebSockets();
     // get database reference
@@ -289,6 +357,7 @@ function Scoreboard(options = null, showErrors = false){
         // clears and re-populates the scoreboard table
         // remove all rows except the headers
         $("#scoreboard").find("tr:gt(0)").remove();
+        $('#loader').show();
         // find the table with the id scoreboard
         var scoreTable = $("#scoreboard");  
         // cool crown for top player :)
@@ -302,6 +371,7 @@ function Scoreboard(options = null, showErrors = false){
             scoreTable.append(newRow);
             crown = "";
         }
+        $('#loader').hide();
     }
 
     var addScore = function(data){
@@ -349,15 +419,24 @@ function Scoreboard(options = null, showErrors = false){
         }
     }
      
-     
+
     $(document).ready(function() {
     // initialize dialogs
     $('#container_scoreboard').dialog({
         // scoreboard dialog settings
         modal:true, //Not necessary but dims the page background
         autoOpen:false,
-        height:400,
+        width:options.scoreboardWidth,
+        height:options.scoreboardHeight,
         position:"top",
+        show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "slide",
+        duration: 1000
+      },
         open:function(){
             //$(this).html('');
             },
@@ -375,7 +454,16 @@ function Scoreboard(options = null, showErrors = false){
         // score submission dialog settings
         modal:true, //Not necessary but dims the page background
         autoOpen:false,
-        height:"auto",
+        width:options.submitDialogWidth,
+        height:options.submitDialogHeight,
+        show: {
+        effect: "slide",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      },
         open:function(){
             //$(this).html('');
             $('#lbl_best_score_submit').text(String(_score));
@@ -401,6 +489,6 @@ function Scoreboard(options = null, showErrors = false){
             ]
         }
     );
-applyStyling(); 
+  applyStyling();
   });
 }
